3d277d7b16a6f41f764ea5e65328f7437d1b6191
erkhem22 <81277600+erkhem22@users.noreply.github.com>
2021-05-10 23:03:36 +0700

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
 print("mongolia is a not china or russian some people thinking like that")
+def mongolian():
+    print("offical language: mongolia")
+    print("population: 3 million")
+    print("neighbour countries: china, russia")
+    print("mongolian word: монгол")
+mongolia()
